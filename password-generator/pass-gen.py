cfd19d65a11bd0f893ac137481a21a2be665d6bf
Vincenty82 <143327758+Vincenty82@users.noreply.github.com>
2023-09-07 18:36:51 +0200

    update pass-gen.py
    -Fixed loop bug where main menu was called twice.
    -Added support for changing the password length.

--- password-generator/pass-gen.py
+++ password-generator/pass-gen.py
@@ -1,20 +1,21 @@
 #modules
 
+import os
+import platform
 import random
 import string
 import sys
 
 #variables
 
-password = str()                                                            # Reserve password as string
-character_str = string.digits + string.ascii_letters + string.punctuation   # Define character string to choose from
-password_length = 16                                                        # Define default password lenght
-formatting_reset = "\033[0;0m"                                              # ANSI SGR control sequence
-main_loop_check = True
+character_str = string.digits + string.ascii_letters + string.punctuation       # Define character string to choose from.
+pswrd_len = 16                                                                  # Define default password lenght.
+formatting_reset = "\033[0;0m"                                                  # ANSI SGR control sequence   -> back to default formatting <-
+main_loop_check = True                                                          # Reserve main menu loop check initial state. 
 
 #classes
 
-class fg_color:                                                             # ANSI SGR control sequence
+class fg_color:                                                                 # ANSI SGR control sequence   -> foreground color <-   *_0 = reset previous formatting   *_1 = keep previous formatting
     black_0 = "\033[0;30m"
     red_0 = "\033[0;31m"
     green_0 = "\033[0;32m"
@@ -32,7 +33,7 @@ class fg_color:                                                             # AN
     cyan_1 = "\033[1;36m"
     white_1 = "\033[1;37m"
 
-class bg_color:                                                             # ANSI SGR control sequence
+class bg_color:                                                                 # ANSI SGR control sequence   -> background color <-   *_0 = reset previous formatting   *_1 = keep previous formatting
     black_0 = "\033[0;40m"
     red_0 = "\033[0;41m"
     green_0 = "\033[0;42m"
@@ -55,27 +56,44 @@ class bg_color:                                                             # AN
 def main_menu():
     sys.stdout.write(fg_color.green_0)
     print("\n1. Generate password.")
-    print("2. Set password lenght.")
+    print("2. Set password length.")
     print("3. Exit.\n")
     sys.stdout.write(formatting_reset)
-    return int(input("Type the option number and press Enter:"))
+    while True:
+        try:
+            choice = int(input(fg_color.cyan_0 + os.getlogin() + "@" + platform.system() + ":> " + formatting_reset))
+            break
+        except ValueError:
+            print(fg_color.red_0 + "\nOnly nubers are supported.\n" + formatting_reset)
+    return choice
 
-def generate_password(a):
-    password = str()
-    for i in range(a):
+def generate_password(password_length):                                                                                                 # Generate password
+    password = str()                                                                                                                    # Reserve password as string
+    for i in range(password_length):
         password = password + (random.choice(character_str))
+    print(fg_color.cyan_0 + "\nThis is your " + f"{password_length}" + " character long password:" + formatting_reset)
+    print(password)
     return password
+
+def password_length():                                                                                                                  # Set password length
+    while True:
+            try:
+                password_length = int(input("\n" + fg_color.cyan_0 + "What is the desired password lenght? " + formatting_reset))
+                break
+            except ValueError:
+                print(fg_color.red_0 + "\nOnly nubers are supported." + formatting_reset)
+    return password_length
 
 #program
 
+print("\n" + bg_color.cyan_0 + "Welcome to the password generator. Choose what you would like to do next." + formatting_reset)
 while main_loop_check: 
-    if main_menu() == 1:
-        print(f"\n{generate_password(password_length)}")
-    elif main_menu() == 2:
-        password_length = int(input("\nWhat is the desired password lenght?:"))
-    else:
+    main_menu_check = main_menu()
+    if main_menu_check == 1:
+        pswrd = generate_password(pswrd_len)
+    elif main_menu_check == 2:
+        pswrd_len = password_length()
+    elif main_menu_check == 3:
         main_loop_check = False
-
-#wynik
-
-# print(password)
+    else:
+        print(fg_color.red_0 + "\nChoose one of the available options." + formatting_reset)
